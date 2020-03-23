1c3739040af9848376f1f8b1bc5d094c82d677f2
Michał Piotrowski <97.piotrowski.michal@gmail.com>
2020-03-23 20:58:13 +0100

    Initial commit

---
+++ desktop/src/pl/psk/gkproject/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.psk.gkproject.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.psk.gkproject.PlatformGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlatformGame(), config);
+	}
+}

cba839e49941923b6dfc1854e24275a7ae5c3eda
Michał Piotrowski <97.piotrowski.michal@gmail.com>
2020-03-24 19:50:07 +0100

    Add game screen class

---
+++ core/src/pl/psk/gkproject/screens/PlayScreen.java
@@ -0,0 +1,54 @@
+package pl.psk.gkproject.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import pl.psk.gkproject.PlatformGame;
+
+public class PlayScreen implements Screen {
+    private PlatformGame game;
+    Texture texture = new Texture("badlogic.jpg");
+
+    public PlayScreen(PlatformGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.getBatch().begin();
+        game.getBatch().draw(texture, 0, 0);
+        game.getBatch().end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}

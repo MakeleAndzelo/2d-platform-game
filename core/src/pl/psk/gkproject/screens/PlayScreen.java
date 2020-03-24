264630339b43b44dc7631a5c0b970d03ef31f8b7
Michał Piotrowski <97.piotrowski.michal@gmail.com>
2020-03-24 22:06:14 +0100

    Change viewport type

--- core/src/pl/psk/gkproject/screens/PlayScreen.java
+++ core/src/pl/psk/gkproject/screens/PlayScreen.java
@@ -3,15 +3,22 @@ package pl.psk.gkproject.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import pl.psk.gkproject.PlatformGame;
 
 public class PlayScreen implements Screen {
-    private PlatformGame game;
+    private final PlatformGame game;
+    private final OrthographicCamera gameCamera = new OrthographicCamera();
+    private final Viewport gameViewport;
     Texture texture = new Texture("badlogic.jpg");
 
     public PlayScreen(PlatformGame game) {
         this.game = game;
+        this.gameViewport = new FitViewport(800, 480, gameCamera);
     }
 
     @Override
@@ -22,6 +29,7 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.getBatch().setProjectionMatrix(gameCamera.combined);
         game.getBatch().begin();
         game.getBatch().draw(texture, 0, 0);
         game.getBatch().end();
@@ -29,7 +37,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        gameViewport.update(width, height);
     }
 
     @Override
